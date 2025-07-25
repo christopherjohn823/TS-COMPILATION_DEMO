56a461a85a53a5a9bb8acc44a56bdab4ec9e11c7
Christopher John <christopher.john823@gmail.com>
2025-07-25 19:35:57 -0400

    changes on July 25th

--- thiskeyword.ts
+++ thiskeyword.ts
@@ -8,5 +8,5 @@ class TestRunner {
 const testRunner = new TestRunner(); 
 testRunner.run();
 const runner = new TestRunner();
-setTimeout( runner.run, 2000);  // when you use callback then the 'this' property is lost, therefore use arrow function
+//setTimeout( runner.run, 2000);  // when you use callback then the 'this' property is lost, therefore use arrow function
 setTimeout(() => runner.run(), 2000);
